9cd4b56f4116e5b9d825aae59787c146799f46a6
Stian Lockhart Pedersen <stilo759@student.liu.se>
2023-09-21 15:41:36 +0200

    first

---
+++ build/CMakeFiles/0_Bootstrap.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "0_Bootstrap"
+  "0_Bootstrap.pdb"
+  "CMakeFiles/0_Bootstrap.dir/Bootstrap.o"
+  "CMakeFiles/0_Bootstrap.dir/Bootstrap.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/0_Bootstrap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
