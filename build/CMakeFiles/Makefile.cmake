128152443861ecf451698894a0121d62d5adef0c
Stian Lockhart Pedersen <stilo759@student.liu.se>
2023-09-29 12:01:39 +0800

    tutorial 2 done

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -48,5 +48,6 @@ set(CMAKE_MAKEFILE_PRODUCTS
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/0_Bootstrap.dir/DependInfo.cmake"
+  "CMakeFiles/Tutorial_2.dir/DependInfo.cmake"
   "CMakeFiles/OgreBasicApp.dir/DependInfo.cmake"
   )
---
+++ build/CMakeFiles/Tutorial_2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tutorial_2.dir/src/BasicTutorial_2.o"
+  "CMakeFiles/Tutorial_2.dir/src/BasicTutorial_2.o.d"
+  "Tutorial_2"
+  "Tutorial_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tutorial_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
